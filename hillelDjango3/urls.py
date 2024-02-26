b92f20c7e4aecf989e2debec2ef38b541b9d1fb9
p1v2 <pavliuk96@gmail.com>
2024-02-26 20:14:39 +0200

    google sheets

--- hillelDjango3/urls.py
+++ hillelDjango3/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
+from hillelDjango3.views import registration, obtain_auth_token
 from products.views import celery_view
 from products.viewsets import ProductViewSet, OrderViewSet, RecipeViewSet
 from telegram.views import telegram
@@ -32,5 +33,8 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('telegram/', telegram),
     path('celery/', celery_view),
+    path('api-registration/', registration),
+    path('api-auth/', obtain_auth_token),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
+
