fa7d34a62c7749c0e386a5c3fdcadf72e9dabcf4
p1v2 <pavliuk96@gmail.com>
2024-03-04 21:05:19 +0200

    docker setup

--- hillelDjango3/settings.py
+++ hillelDjango3/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",") + ["0.0.0.0"]
 
 
 # Application definition
