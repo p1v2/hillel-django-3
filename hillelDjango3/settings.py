0d622a86f9301741abcb6615fe3fab92a6d7b9c8
p1v2 <pavliuk96@gmail.com>
2024-03-11 20:34:05 +0200

    add DEBUG=False

--- hillelDjango3/settings.py
+++ hillelDjango3/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",") + ["0.0.0.0"]
 
