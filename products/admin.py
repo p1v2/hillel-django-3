07440a74699eebd65dd52733968160e16159cb14
pashkovuch <dmitriypqskovs@gmail.com>
2024-03-17 13:33:51 +0200

    Start

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from products.models import Product, Category, Recipe, Order, OrderProduct, StoreInventory, Store
+from products.models import Product, Category, Recipe, Order, OrderProduct
 
 # Register your models here.
 admin.site.register(Product)
