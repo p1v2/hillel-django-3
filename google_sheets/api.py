bbddcc303b791bb7d513e0d2d7b9de0f5d31cf1d
p1v2 <pavliuk96@gmail.com>
2024-04-01 20:12:44 +0300

    Github auth

--- google_sheets/api.py
+++ google_sheets/api.py
@@ -2,6 +2,7 @@ import os.path
 
 import gspread
 from google.oauth2.service_account import Credentials
+from google.oauth2.credentials import Credentials as UserCredentials
 
 
 CREDENTIALS_PATH = os.path.join(os.path.dirname(__file__), "credentials.json")
@@ -21,12 +22,28 @@ def get_client():
     return client
 
 
-def write_to_sheet(range_name, data):
-    client = get_client()
+def get_user_client(user):
+    token = user.socialaccount_set.get(provider="google").socialtoken_set.get().token
+    refresh_token = user.socialaccount_set.get(provider="google").socialtoken_set.get().token_secret
+
+    credentials = UserCredentials(
+        token=token,
+        refresh_token=refresh_token,
+        token_uri="https://oauth2.googleapis.com/token",
+        scopes=["https://www.googleapis.com/auth/spreadsheets"]
+    )
+
+    client = gspread.authorize(credentials)
+
+    return client
+
+
+def write_to_sheet(sheet_name, range_name, data, user=None):
+    client = get_user_client(user) if user else get_client()
 
     spreasheet = client.open("Hillel Django 3")
 
-    sheet = spreasheet.sheet1
+    sheet = spreasheet.worksheet(sheet_name)
 
     sheet.update(range_name, data)
 
